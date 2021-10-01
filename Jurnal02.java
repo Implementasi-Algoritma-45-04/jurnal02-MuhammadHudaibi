318d9d2c8d3d5609c4d82058c989a07584449c94
MuhammmadHudibi <muhhudaibi11@gmail.con>
2021-10-01 14:09:24 +0700

    Solusi jurnal 02

---
+++ Jurnal02.java
@@ -0,0 +1,7 @@
+public class Jurnal02 {
+	public static void main(String[] args) {
+		System.out.println("NIM	: 6706213010") ;
+		System.out.println("Nama	: Muhammad Hudaibi") ;
+		System.out.println("Kelas	: D3IF-45-04") ;
+	}
+}
